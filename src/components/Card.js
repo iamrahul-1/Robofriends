6be168cb75be5adfa013e4e1385daa295df98daa
Rahul <rchauhan8550@gmail.com>
2023-08-23 14:10:19 +0530

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Card = ({name,email,id}) => {
+  return (
+    <div className= "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt = "Robots" src = {`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
+
+
+// const alpha = function test (a ,b ) {
+//   let myName = `Hello Mother fucker ${a} something ${b}`
+//   console.log(myName);
+// }
+
+// alpha(`rahul`, `gadha`);
